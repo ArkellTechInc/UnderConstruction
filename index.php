328135fe5da8a17022519963742e793c820deb64
cylindersmash <34405512+cylindersmash@users.noreply.github.com>
2018-02-15 15:46:57 -0500

    changed image and index.html is now index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<title>Arkell Tech Inc</title>
+<meta name="description" content="Local software, application and web development in Hamilton, Ontario.">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+<body>
+<div class="container">
+	<img class ="bgImage" src="images/mansworkinsmall.jpg" alt="arkell tech inc">
+	<img class="logo" src="images/logo.png" alt="arkell tech inc logo">
+	<div class="topSpacer">
+		<div class="bebas centeredText" id="title">site under construction</div>
+		<div class="anon centeredText" id="subTitle"><a href="mailto:arkelltech2017@gmail.com?Subject=Website%20Inquiry" target="_top">info@arkelltech.com</a></div>
+	</div>
+</div> 
+</body>
+</html>
